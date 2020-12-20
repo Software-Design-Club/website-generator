27e8fe56d2d65189762c08933c7bfd99ffabea76
Emmanuel Genard <edgenard@gmail.com>
2020-12-20 12:52:00 -0500

    Create a directory given a website name

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,19 @@
 const WebsiteGenerator = require('../lib/index')
+const fs = require('fs')
+// Site name: awesomeco
+// Author: Max Power​ Do you want a folder for JavaScript? y
+// Do you want a folder for CSS? y
+// Created ./awesomeco
+// Created ./awesomeco/index.html
+// Created ./awesomeco/js/
+// Created ./awesomeco/css/
 
-describe('WebisteGenerator', () => {
-  it('runs', () => {
-    expect(WebsiteGenerator.run()).toEqual(true)
+describe('WebsiteGenerator', () => {
+  it('creates new directory using given website name', () => {
+    const websiteGenerator = new WebsiteGenerator()
+
+    websiteGenerator.generate('myWebsite')
+    const directory = fs.existsSync('./myWebsite')
+    expect(directory).toBeTruthy()
   })
 })
