1e7de1e1e28a6f26ec86a54e772f0f7c6fd3ac5f
Emmanuel Genard <edgenard@gmail.com>
2021-01-03 13:06:05 -0500

    Prompt user for website name using readline

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,6 @@
 const WebsiteGenerator = require('../lib/index')
 const fs = require('fs')
+const readline = require('readline')
 // Site name: awesomeco
 // Author: Max Power
 // Do you want a folder for JavaScript? y
@@ -14,37 +15,56 @@ describe('WebsiteGenerator', () => {
   afterEach(() => {
     fs.rmdirSync(`./${websiteName}`, { recursive: true, force: true })
   })
-  it('creates new directory using given website name', () => {
-    const websiteGenerator = new WebsiteGenerator()
 
-    websiteGenerator.generate(websiteName)
-    const directory = fs.existsSync(`./${websiteName}`)
-    expect(directory).toBeTruthy()
+  describe('generate', () => {
+    it('creates new directory using given website name', () => {
+      const websiteGenerator = new WebsiteGenerator()
+
+      websiteGenerator.generate(websiteName)
+      const directory = fs.existsSync(`./${websiteName}`)
+      expect(directory).toBeTruthy()
+    })
+
+    it('creates an index.html file in the website directory', () => {
+      const websiteGenerator = new WebsiteGenerator()
+
+      websiteGenerator.generate(websiteName)
+
+      const htmlFile = fs.existsSync(`./${websiteName}/index.html`)
+      expect(htmlFile).toBeTruthy()
+    })
+
+    it('creates a valid html file', () => {
+      const websiteGenerator = new WebsiteGenerator()
+
+      websiteGenerator.generate(websiteName)
+
+      const fileContents = fs.readFileSync(`./${websiteName}/index.html`, { encoding: 'utf-8' })
+
+      expect(fileContents).toMatch(/^<html>\s*<head>(.|\s)*<\/head>(\s)*<body>(.|\s)*<\/body>(\s)*<\/html>$/gm)
+    })
+
+    it('index has the website name as title', () => {
+      const websiteGenerator = new WebsiteGenerator()
+      websiteGenerator.generate(websiteName)
+      const fileContents = fs.readFileSync(`./${websiteName}/index.html`, { encoding: 'utf-8' })
+      expect(fileContents).toContain(`<title>${websiteName}</title>`)
+    })
   })
 
-  it('creates an index.html file in the website directory', () => {
-    const websiteGenerator = new WebsiteGenerator()
+  describe('prompt', () => {
+    it('passes the user input to Website Generator', () => {
+      const ui = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      })
+      const websiteGenerator = new WebsiteGenerator(ui)
+      websiteGenerator.generate = jest.fn()
 
-    websiteGenerator.generate(websiteName)
+      websiteGenerator.prompt()
+      ui.write('my_website\n')
 
-    const htmlFile = fs.existsSync(`./${websiteName}/index.html`)
-    expect(htmlFile).toBeTruthy()
-  })
-
-  it('creates a valid html file', () => {
-    const websiteGenerator = new WebsiteGenerator()
-
-    websiteGenerator.generate(websiteName)
-
-    const fileContents = fs.readFileSync(`./${websiteName}/index.html`, { encoding: 'utf-8' })
-
-    expect(fileContents).toMatch(/^<html>\s*<head>(.|\s)*<\/head>(\s)*<body>(.|\s)*<\/body>(\s)*<\/html>$/gm)
-  })
-
-  it('index has the website name as title', () => {
-    const websiteGenerator = new WebsiteGenerator()
-    websiteGenerator.generate(websiteName)
-    const fileContents = fs.readFileSync(`./${websiteName}/index.html`, { encoding: 'utf-8' })
-    expect(fileContents).toContain(`<title>${websiteName}</title>`)
+      expect(websiteGenerator.generate).toHaveBeenCalledWith('my_website')
+    })
   })
 })
