f835426e18429926d9e78898364fa8bd73713517
Emamnuel <emmanuel@teepublic.com>
2021-02-13 17:54:25 -0500

    Add basic expressjs server

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const https = require('http')
+const express = require('express')
+const app = express()
+const port = 3000
 
-const port = 3001
-
-const server = https.createServer((request, response) => {
-  response.status = 200
-  response.setHeader('Content-Type', 'text/html')
-  response.end('<h1> Hello World! </h1>')
-  console.log(request);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+  console.log(req);
 })
 
-server.listen(port, () => {
-  console.log('server running on ', port)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
