ec51b7988a38c3a18898bc365ea59a01968280df
Emamnuel <emmanuel@teepublic.com>
2021-02-14 13:22:52 -0500

    latest changes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const WebsiteGenerator = require('../lib/websiteGenerator')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.post('/', function(req, res, next){
-  console.log('GETTING HERE');
-  res.send('Request Received')
+  res.contentType('application/octet-stream')
+  res.sendFile('/Users/emamnuel/Workspace/website-generator/public/stylesheets/style.css')
 
 });
 
