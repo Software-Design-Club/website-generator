0938103f6c3b4588b09383751614d60dc084e570
Kirsten Nordine <kirstennordine@gmail.com>
2021-02-20 13:39:19 -0500

    Use node-archiver to generate a zip and send back with the response

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+
+const fs = require('fs')
+const archiver = require('archiver')
+
 const WebsiteGenerator = require('../lib/websiteGenerator')
 
 /* GET home page. */
@@ -7,9 +11,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.post('/', function(req, res, next){
-  res.contentType('application/octet-stream')
-  res.sendFile('/Users/emamnuel/Workspace/website-generator/public/stylesheets/style.css')
+  // Add zip archive attachment to the response
+  res.attachment('website.zip')
 
+  const zip = archiver('zip')
+
+  // Pipe zip data to the response
+  zip.pipe(res)
+
+  const cssPath = '/Users/stride-admin/dev/website-generator/public/stylesheets/style.css'
+
+  zip
+    .append('hello world', { name: 'hello.txt' }) // Create a new text file and add it
+    .append("console.log('Hello world')", { name: 'js/script.js' }) // Create a js file in a nested directory and add that
+    .append(fs.createReadStream(cssPath), { name: 'css/styles.css' }) // Read an existing file and add that
+    .finalize() // finalize the archive
 });
 
 module.exports = router;
