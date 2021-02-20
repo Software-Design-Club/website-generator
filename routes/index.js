551c0a24365ffeb88a19b6e5aa54e47cdbf9f175
Kirsten Nordine <kirstennordine@gmail.com>
2021-02-20 13:13:14 -0500

    Use JSZip to generate and download zip archive (wip)
    This doesn't quite work yet. The downloaded zip can't be unzipped - I
    think there is nothing in it.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs')
+const JSZip = require('jszip')
 const WebsiteGenerator = require('../lib/websiteGenerator')
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.post('/', function(req, res, next){
-  res.contentType('application/octet-stream')
-  res.sendFile('/Users/emamnuel/Workspace/website-generator/public/stylesheets/style.css')
 
+router.post('/', function(req, res, next){
+  res.attachment('website.zip')
+
+  const zip = new JSZip()
+  zip.file('hello.txt', 'Hello world!')
+  zip
+    .generateNodeStream({ streamFiles:true })
+    .pipe(fs.createWriteStream('website.zip'))
+    .on('finish', content => {
+      console.log('content: ', content)
+      res.end()
+    })
 });
 
 module.exports = router;
