0682e16b21319cc11ce92d5d579f11743309c40e
Emmanuel Genard <edgenard@gmail.com>
2021-02-28 13:31:11 -0500

    Redesign prompt to return website options object

--- test/cli.test.js
+++ test/cli.test.js
@@ -2,31 +2,19 @@ const CLI = require('../lib/cli.js')
 const readline = require('readline')
 
 describe('CLI', () => {
-  it('prompts for site name', () => {
-    const io = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    })
-
-    io.question = jest.fn()
-
-    const cli = new CLI(io)
-    io.question = jest.fn()
-
-    cli.prompt('Site Name: ')
-    expect(io.question.mock.calls[0][0]).toBe('Site Name: ')
-    io.question.mockRestore()
-  })
-
-  it('stores the answers to the prompt', async () => {
+  it('returns a users desired website configuration', async () => {
     const io = readline.createInterface({
       input: process.stdin,
       output: process.stdout
     })
     const cli = new CLI(io)
     setImmediate(() => io.write('My Website\n'))
-    await cli.prompt('Site Name: ')
+    const configuration = {
+      siteName: 'Site Name: '
+    }
 
-    expect(cli.websiteOptions).toEqual({ siteName: 'My Website' })
+    const websiteOptions = await cli.prompt(configuration)
+
+    expect(websiteOptions).toEqual({ siteName: 'My Website' })
   })
 })
