da30a35c6e5e6003c3950ba31b69a5f90ef18f71
Emmanuel Genard <edgenard@gmail.com>
2021-03-07 13:19:39 -0500

    Simplify CLI

--- test/cli.test.js
+++ test/cli.test.js
@@ -3,11 +3,12 @@ const readline = require('readline')
 
 describe('CLI', () => {
   it('returns a users desired website configuration', async () => {
+    const generator = { generate: jest.fn() }
     const io = readline.createInterface({
       input: process.stdin,
       output: process.stdout
     })
-    const cli = new CLI(io)
+    const cli = new CLI(io, generator)
     setImmediate(() => io.write('My Website\n'))
     setImmediate(() => io.write('Name\n'))
     const promptConfigurations = {
@@ -15,8 +16,10 @@ describe('CLI', () => {
       authorName: 'Author: '
     }
 
-    const websiteOptions = await cli.generateWebsiteOptions(promptConfigurations)
+    await cli.generateWebsite(promptConfigurations)
 
-    expect(websiteOptions).toEqual({ siteName: 'My Website', authorName: 'Name' })
+    const options = { siteName: 'My Website', authorName: 'Name' }
+
+    expect(generator.generate).toHaveBeenCalledWith(options)
   })
 })
