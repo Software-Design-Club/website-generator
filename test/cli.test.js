397e35cf4d0eaac134e5546742f0ddd183be4118
Emmanuel Genard <edgenard@gmail.com>
2021-03-07 13:53:00 -0500

    Output generated files from WebisteGenerator

--- test/cli.test.js
+++ test/cli.test.js
@@ -3,11 +3,12 @@ const readline = require('readline')
 
 describe('CLI', () => {
   it('calls generator with user input', async () => {
-    const generator = { generate: jest.fn() }
+    const generator = { generate: jest.fn().mockResolvedValue([]) }
     const io = readline.createInterface({
       input: process.stdin,
       output: process.stdout
     })
+
     const cli = new CLI(io, generator)
     setImmediate(() => io.write('My Website\n'))
     setImmediate(() => io.write('Name\n'))
@@ -22,4 +23,25 @@ describe('CLI', () => {
 
     expect(generator.generate).toHaveBeenCalledWith(options)
   })
+
+  it('outputs generated', async () => {
+    const generator = { generate: jest.fn() }
+    const io = {
+      write: jest.fn()
+    }
+    const cli = new CLI(io, generator)
+
+    io.write = jest.fn()
+
+    cli.outputGeneratedFiles(['path1', 'path2'])
+
+    expect(io.write.mock.calls[0][0]).toEqual('Created path1')
+    expect(io.write.mock.calls[1][0]).toEqual('Created path2')
+  })
+  // Created ./awesomeco
+// Created ./awesomeco/index.html
+// Created ./awesomeco/js/
+// Created ./awesomeco/js/script.js
+// Created ./awesomeco/css/
+// Created ./awesomeco/css/styles.css
 })
