54ed82eabfd092402132a5668ecff0b6f5bb4ad6
Emmanuel Genard <edgenard@gmail.com>
2021-03-14 12:48:24 -0400

    WebsiteGenerator.generate changed to take in object; Remove all UI code from WebsiteGenerator

--- test/websiteGenerator.test.js
+++ test/websiteGenerator.test.js
@@ -1,27 +1,13 @@
 
 const WebsiteGenerator = require('../lib/websiteGenerator')
 const fs = require('fs')
-const readline = require('readline')
-// Site name: awesomeco
-// Author: Max Power
-// Do you want a folder for JavaScript? y
-// Do you want a folder for CSS? y
-// Created ./awesomeco
-// Created ./awesomeco/index.html
-// Created ./awesomeco/js/
-// Created ./awesomeco/js/script.js
-// Created ./awesomeco/css/
-// Created ./awesomeco/css/styles.css
 
 describe('WebsiteGenerator', () => {
   const websiteName = 'myWebsite'
-  const ui = {
-    write: jest.fn()
-  }
   let websiteGenerator
 
   beforeEach(() => {
-    websiteGenerator = new WebsiteGenerator(ui)
+    websiteGenerator = new WebsiteGenerator()
   })
 
   afterEach(() => {
@@ -30,21 +16,21 @@ describe('WebsiteGenerator', () => {
 
   describe('generate', () => {
     it('creates new directory using given website name', async () => {
-      await websiteGenerator.generate(websiteName)
+      await websiteGenerator.generate({ siteName: websiteName })
 
       const directory = fs.existsSync(`./${websiteName}`)
       expect(directory).toBeTruthy()
     })
 
     it('creates an index.html file in the website directory', async () => {
-      await websiteGenerator.generate(websiteName)
+      await websiteGenerator.generate({ siteName: websiteName })
 
       const htmlFile = fs.existsSync(`./${websiteName}/index.html`)
       expect(htmlFile).toBeTruthy()
     })
 
     it('creates a valid html file', async () => {
-      await websiteGenerator.generate(websiteName)
+      await websiteGenerator.generate({ siteName: websiteName })
 
       const fileContents = fs.readFileSync(`./${websiteName}/index.html`, { encoding: 'utf-8' })
 
@@ -52,140 +38,62 @@ describe('WebsiteGenerator', () => {
     })
 
     it('index has the website name as title', async () => {
-      await websiteGenerator.generate(websiteName)
+      await websiteGenerator.generate({ siteName: websiteName })
 
       const fileContents = fs.readFileSync(`./${websiteName}/index.html`, { encoding: 'utf-8' })
       expect(fileContents).toContain(`<title>${websiteName}</title>`)
     })
 
     it('includes the author name in the html meta tag', async () => {
-      await websiteGenerator.generate(websiteName, 'Bobby')
+      await websiteGenerator.generate({ siteName: websiteName, authorName: 'Bobby' })
 
       const fileContents = fs.readFileSync(`./${websiteName}/index.html`, { encoding: 'utf-8' })
       expect(fileContents).toContain('<meta author="Bobby" />')
     })
 
-    it('shows the user the path of the directory', async () => {
-      await websiteGenerator.generate(websiteName)
-
-      expect(ui.write).toHaveBeenCalledWith(`Created ./${websiteName}\n`)
-    })
-
-    it('shows the user the path to the html file', async () => {
-      await websiteGenerator.generate(websiteName)
-
-      expect(ui.write).toHaveBeenCalledWith(`Created ./${websiteName}/index.html\n`)
-    })
-
     describe('Creating JS directory and file', () => {
       it('generates a JS directory if user wants it', async () => {
-        await websiteGenerator.generate(websiteName, 'Something', true)
+        await websiteGenerator.generate({ siteName: websiteName, generateJS: true })
 
         const directory = fs.existsSync(`./${websiteName}/js`)
         expect(directory).toBeTruthy()
       })
 
       it('does not generate a JS directory if user opts out', async () => {
-        await websiteGenerator.generate(websiteName, 'Something', false)
+        await websiteGenerator.generate({ siteName: websiteName, generateJS: false })
 
         const directory = fs.existsSync(`./${websiteName}/js`)
         expect(directory).toBeFalsy()
       })
 
-      it('shows the user the path to the JS directory', async () => {
-        await websiteGenerator.generate(websiteName, null, true)
-
-        expect(ui.write).toHaveBeenCalledWith(`Created ./${websiteName}/js\n`)
-      })
-
       it('creates a JS file', async () => {
-        await websiteGenerator.generate(websiteName, null, true)
+        await websiteGenerator.generate({ siteName: websiteName, generateJS: true })
 
         const jsFile = fs.existsSync(`./${websiteName}/js/script.js`)
         expect(jsFile).toBeTruthy()
-      })
-
-      it('shows the user the path to the JS file', async () => {
-        await websiteGenerator.generate(websiteName, null, true)
-
-        expect(ui.write).toHaveBeenCalledWith(`Created ./${websiteName}/js/script.js\n`)
       })
     })
 
     describe('Creating CSS directory and file', () => {
       it('generates a CSS directory if user wants it', async () => {
-        await websiteGenerator.generate(websiteName, 'Something', false, true)
+        await websiteGenerator.generate({ siteName: websiteName, generateCSS: true })
 
         const directory = fs.existsSync(`./${websiteName}/css`)
         expect(directory).toBeTruthy()
       })
 
       it('does not generate a CSS directory if user opts out', async () => {
-        await websiteGenerator.generate(websiteName, 'Something', false, false)
+        await websiteGenerator.generate({ siteName: websiteName, generateCSS: false })
 
         const directory = fs.existsSync(`./${websiteName}/css`)
         expect(directory).toBeFalsy()
       })
 
-      it('shows the user the path to the CSS directory', async () => {
-        await websiteGenerator.generate(websiteName, null, false, true)
-
-        expect(ui.write).toHaveBeenCalledWith(`Created ./${websiteName}/css\n`)
-      })
-
       it('creates a CSS file', async () => {
-        await websiteGenerator.generate(websiteName, null, false, true)
+        await websiteGenerator.generate({ siteName: websiteName, generateCSS: true })
 
         const jsFile = fs.existsSync(`./${websiteName}/css/style.css`)
         expect(jsFile).toBeTruthy()
-      })
-
-      it('shows the user the path to the CSS file', async () => {
-        await websiteGenerator.generate(websiteName, null, false, true)
-
-        expect(ui.write).toHaveBeenCalledWith(`Created ./${websiteName}/css/style.css\n`)
-      })
-    })
-  })
-
-  describe('prompt', () => {
-    describe('when the uses chooses to include js and css', () => {
-      it('passes the user input to Website Generator', async () => {
-        const ui = readline.createInterface({
-          input: process.stdin,
-          output: process.stdout
-        })
-        const websiteGenerator = new WebsiteGenerator(ui)
-        websiteGenerator.generate = jest.fn()
-
-        setImmediate(() => ui.write('my_website\n'))
-        setImmediate(() => ui.write('Name\n'))
-        setImmediate(() => ui.write('Y\n'))
-        setImmediate(() => ui.write('Y\n'))
-
-        await websiteGenerator.prompt()
-
-        expect(websiteGenerator.generate).toHaveBeenCalledWith('my_website', 'Name', true, true)
-      })
-    })
-
-    describe('when the uses chooses to exclude js and css', () => {
-      it('passes converts the user\'s js selection and passes it to the generator', async () => {
-        const ui = readline.createInterface({
-          input: process.stdin,
-          output: process.stdout
-        })
-        const websiteGenerator = new WebsiteGenerator(ui)
-        websiteGenerator.generate = jest.fn()
-
-        setImmediate(() => ui.write('my_website\n'))
-        setImmediate(() => ui.write('Name\n'))
-        setImmediate(() => ui.write('N\n'))
-        setImmediate(() => ui.write('N\n'))
-
-        await websiteGenerator.prompt()
-
-        expect(websiteGenerator.generate).toHaveBeenCalledWith('my_website', 'Name', false, false)
       })
     })
   })
