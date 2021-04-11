7882c16ef1eaf96a1333492fdefdc63650042f20
Emmanuel Genard <edgenard@gmail.com>
2021-04-11 12:59:07 -0400

    Return zip archive attachment with response

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,9 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' })
 })
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   new GeneratorService(req.body)
+  res.writeHead(200, { 'Content-Type': 'application/zip', 'Content-disposition': `attachment;filename=${'my_website.zip'}` })
   res.end()
 })
 
--- test/routes/index.test.js
+++ test/routes/index.test.js
@@ -17,4 +17,12 @@ describe('/', () => {
 
     expect(GeneratorService).toHaveBeenCalledWith(transformedInput)
   })
+
+  it('sends  a file to the user', async () => {
+    await request.post('/')
+      .send('siteName=my_website')
+      .expect('Content-Type', 'application/zip')
+      .expect('Content-disposition', 'attachment;filename=my_website.zip')
+      .expect(200)
+  })
 })
