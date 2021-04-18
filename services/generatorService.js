0c8fc6e054b78ec79fb0c42da6f078a4ae7cce1c
Emmanuel Genard <edgenard@gmail.com>
2021-04-18 14:01:35 -0400

    Create Zip archive with files generated by WebsiteGenerator

--- services/generatorService.js
+++ services/generatorService.js
@@ -1,4 +1,6 @@
 const WebsiteGenerator = require('../lib/websiteGenerator')
+const archiver = require('archiver')
+const fs = require('fs')
 
 class GeneratorService {
   constructor (userConfig, websiteGenerator) {
@@ -6,8 +8,18 @@ class GeneratorService {
     this.websiteGenerator = websiteGenerator || new WebsiteGenerator()
   }
 
-  generateArchive () {
-    this.websiteGenerator.generate(this.userConfig)
+  async generateArchive () {
+    const createdFiles = await this.websiteGenerator.generate(this.userConfig)
+    const output = fs.createWriteStream(`./${this.userConfig.siteName}.zip`)
+    const zip = archiver('zip')
+    zip.pipe(output)
+    createdFiles.forEach((path, index) => {
+      if (index > 0) {
+        zip.append(fs.createReadStream(path), { name: `${index}` })
+      }
+    })
+    zip.finalize()
+
     return {
       fileName: 'test_website.zip',
       filePath: '/Users/emmanuelgenard/Workspace/website-generator/test/fixtures/test_website.zip'
--- test/services/generatorService.test.js
+++ test/services/generatorService.test.js
@@ -1,15 +1,15 @@
 const GeneratorService = require('../../services/generatorService')
+const fs = require('fs')
 
 describe('GeneratorService', () => {
   describe('generateArchive', () => {
+    const userConfig = {
+      siteName: 'my_website',
+      authorName: 'My Name'
+    }
     it('calls websiteGenerator with the user configuration', () => {
       const mockWebsiteGenerator = {
-        generate: jest.fn()
-      }
-
-      const userConfig = {
-        siteName: 'my_website',
-        authorName: 'My Name'
+        generate: jest.fn().mockResolvedValue([])
       }
 
       const generatorService = new GeneratorService(userConfig, mockWebsiteGenerator)
@@ -18,5 +18,19 @@ describe('GeneratorService', () => {
 
       expect(mockWebsiteGenerator.generate).toHaveBeenCalledWith({ siteName: 'my_website', authorName: 'My Name' })
     })
+
+    it('generates and saves a zip archive', async () => {
+      const mockWebsiteGenerator = {
+        generate: () => Promise.resolve(['/Users/emmanuelgenard/Workspace/website-generator/test/fixtures/test_website/', '/Users/emmanuelgenard/Workspace/website-generator/test/fixtures/test_website/index.html'])
+      }
+
+      const generatorService = new GeneratorService(userConfig, mockWebsiteGenerator)
+      await generatorService.generateArchive()
+
+      const archiveExists = fs.existsSync(`${userConfig.siteName}.zip`)
+      expect(archiveExists).toBeTruthy()
+    })
+
+    it.todo('returns the file name and file path of the zip archive')
   })
 })
